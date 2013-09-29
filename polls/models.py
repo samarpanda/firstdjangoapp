a966fb4ce5b6d853c3f760524ed304677b93d4b4
Samar Panda <samar@stepout.com>
2013-09-29 08:55:17 +0530

    Updated question and choice admin panel

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
@@ -7,6 +9,13 @@ class Question(models.Model):
 
 	def __unicode__(self):
 		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
