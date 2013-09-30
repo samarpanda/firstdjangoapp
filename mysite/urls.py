26785497325d5574b33272ebf560f0f5c479cc22
Samar Panda <samar@stepout.com>
2013-09-30 15:57:28 +0530

    Nampspacing urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
